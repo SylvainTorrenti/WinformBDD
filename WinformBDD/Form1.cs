ed93017a96b0899be9f91fca43192d0589441498
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-08-22 14:45:47 +0200

    add comment form1.cs 1.0

--- WinformBDD/Form1.cs
+++ WinformBDD/Form1.cs
@@ -5,29 +5,37 @@ namespace WinformBDD
 {
     public partial class frmMain : Form
     {
+        //creation d'un objet db pour utiliser la class db
         db _db = new db();
+        //creation d'une collection d'utilisateur qui permet le binding
         BindingList<Utilisateur> _utils = new();
         public frmMain()
         {
             InitializeComponent();
+            //appel de la méthode InitializeBinding() qui créé les bind (association) entre les éléments
             InitializeBinding();
 
         }
 
         private void btRefresh_Click(object sender, EventArgs e)
         {
+            
             Utilisateur current = bsUtilisateur.Current as Utilisateur;
-
+            //efface la collection pour eviter de la répéter 
             _utils.Clear();
+            //crétion d'une variable qui récupére les donné récupéré grace a la méthode GetUtilisateurs()
             var u = _db.GetUtilisateurs();
+            //ajout des utilisateurs récupéré dans notre binding list
             foreach (Utilisateur utilisateur in u)
                 _utils.Add(utilisateur);
-
+            //permet de récuperer la position de la selection dans le DataGridView
             if (current is not null)
             {
                 bsUtilisateur.Position = _utils.IndexOf(_utils.Where(u => u.Id == current.Id).FirstOrDefault());
             }
         }
+        //methode pour initier les bind
+        //!!!!!!!!!!!! Les bind ne doivent pas être modifier pendant l'application
         private void InitializeBinding()
         {
             _utils = new BindingList<Utilisateur>();
